bbeb5ea06f5244964b8bc656f22cb6a164bcb417
MrJoshGross <142437255+MrJoshGross@users.noreply.github.com>
2023-10-16 18:39:53 -0400

    Update BearcatGraphics.js
    refactor + new toys, star wip

--- assets/BearcatGraphics.js
+++ assets/BearcatGraphics.js
@@ -2,7 +2,7 @@
  * Wrapper class to make canvas drawing easier + provide targeted functionality for educational purposes
  * @author https://github.com/mrjoshgross
  */
-class BearcatGraphics{
+class BearcatGraphics {
     static RADTODEG = Math.PI / 180;
     static EVENT_TYPES = {
         CLICK: "click",
@@ -21,10 +21,10 @@ class BearcatGraphics{
         MOUSEUP: "mouseup",
     };
 
-    constructor(width, height, canvasName){
-        if(width == null) width = 600;
-        if(height == null) height = 450;
-        if(canvasName == null) canvasName = "Test";
+    constructor(width, height, canvasName) {
+        if (width == null) width = 600;
+        if (height == null) height = 450;
+        if (canvasName == null) canvasName = "Test";
         this.canvas = this.#createCanvas(width, height, canvasName);
         this.setFillColor("white");
         this.setBorderColor("black");
@@ -33,9 +33,9 @@ class BearcatGraphics{
         this.fps = 60;
         this.mouseX = -1;
         this.mouseY = -1;
-        this.addEventListener(BearcatGraphics.EVENT_TYPES.MOUSEMOVE, (e)=>{this.mouseX = this.getMouseX(e); this.mouseY = this.getMouseY(e)});
-        this.addEventListener(BearcatGraphics.EVENT_TYPES.MOUSELEAVE, ()=>{this.mouseX = -1; this.mouseY = -1});
-        setInterval(this.update, 1000/this.fps);
+        this.addEventListener(BearcatGraphics.EVENT_TYPES.MOUSEMOVE, (e) => { this.mouseX = this.getMouseX(e); this.mouseY = this.getMouseY(e) });
+        this.addEventListener(BearcatGraphics.EVENT_TYPES.MOUSELEAVE, () => { this.mouseX = -1; this.mouseY = -1 });
+        setInterval(this.update, 1000 / this.fps);
     }
 
     /**
@@ -44,22 +44,22 @@ class BearcatGraphics{
      * @param {string} canvasName 
      * @returns {canvas.getContext}
      */
-    #createCanvas(width, height, canvasName){
-        if(!document.body) this.#createBody();
+    #createCanvas(width, height, canvasName) {
+        if (!document.body) this.#createBody();
         let canvas = this.#buildCanvas(width, height, canvasName);
         document.body.appendChild(canvas);
         console.log("Created canvas sucessfully!");
         this.canvasElement = document.getElementById(canvasName);
         return this.canvasElement.getContext("2d");
     }
-    
+
     /**
      * @param {*} width 
      * @param {*} height 
      * @param {*} canvasName 
      * @returns {canvas}
      */
-    #buildCanvas(width, height, canvasName){
+    #buildCanvas(width, height, canvasName) {
         let canvas = document.createElement("canvas");
         canvas.id = canvasName;
         canvas.width = width;
@@ -69,257 +69,174 @@ class BearcatGraphics{
         canvas.innerHTML = "<p> Use a browser that supports canvases! </p>";
         return canvas;
     }
-    
+
     /**
      * Creates HTML body element if it's missing.
      */
-    #createBody(){
-        let body = document.createElement("body");
-        document.body = body;
-    }
+    #createBody = () => document.body = document.createElement("body");
 
     /**
      * @param {string} color either a supported color name or rgb hex
      */
-    setFillColor(color){
-        this.canvas.fillStyle = color;
-    }
+    setFillColor = (color) => this.canvas.fillStyle = color;
 
     /**
      * @param {string} color either a supported color name or rgb hex
      */
-    setBorderColor(color){
-        this.canvas.strokeStyle = color;
-    }
+    setBorderColor = (color) => this.canvas.strokeStyle = color;
 
-    setOpacity(percent){
-        this.canvas.globalAlpha = percent;
-    }
+    setOpacity = (percent) => this.canvas.globalAlpha = percent;
 
     /**
      * @param {number} thickness 
      */
-    setLineThickness(thickness){
-        this.canvas.lineWidth = thickness;
+    setLineThickness = (thickness) => this.canvas.lineWidth = thickness;
+    
+
+    drawRect(x, y, width, height, style = FILLFRAME, rotation){
+        if (rotation) this.#rotateLocal(x, y, rotation);
+        if(style === FILL) this.canvas.fillRect(x - width / 2, y - height / 2, width, height)
+        else if(style === FRAME) this.canvas.strokeRect(x - width / 2, y - height / 2, width, height)
+        else if(style === FILLFRAME){
+            this.canvas.strokeRect(x - width / 2, y - height / 2, width, height);
+            this.canvas.fillRect(x - width / 2, y - height / 2, width, height);
+        }else console.error("Invalid style type: " + style + "; valid style types are FILL, FRAME, and FILLFRAME");
+        if (rotation) this.resetCanvasRotation();
+    }
+    
+    drawSquare = (x, y, length, style = FILLFRAME, rotation) => this.drawRect(x, y, length, length, style, rotation);
+
+    drawOval(x, y, xRadius, yRadius, style = FILLFRAME, rotation = 0){
+        if(rotation) this.#rotateLocal(x, y, rotation);
+        this.canvas.beginPath();
+        this.canvas.ellipse(x, y, xRadius, yRadius, rotation * BearcatGraphics.RADTODEG, 0, 2 * Math.PI);
+        this.canvas.fill();
+        if(style === FILL) this.canvas.fill();
+        else if(style === FRAME) this.canvas.stroke();
+        else if(style === FILLFRAME){
+            this.canvas.stroke();
+            this.canvas.fill();
+        }
+        else console.error("Invalid style type: " + style + "; valid style types are FILL, FRAME, and FILLFRAME");
+        if(rotation) this.resetCanvasRotation();
+    }
+    
+    drawCircle = (x, y, radius, style = FILLFRAME) => this.drawOval(x, y, radius, radius, style);
+
+    drawEquilateralTriangle(x, y, length, style = FILLFRAME, rotation) {
+        let h = Math.sqrt(3 * length * length) / 2;
+        let p1 = new Point(x - (length / 2), y + (h / 3));
+        let p2 = new Point(x, y - (2 * h / 3));
+        let p3 = new Point(x + (length / 2), y + (h / 3));
+        this.#drawTriangle(x, y, p1, p2, p3, style, rotation);
     }
 
-    /**
-     * Draws a filled rectangle centered at (x,y).
-     * Color is the canvas' fillStyle.
-     * @param {number} x 
-     * @param {number} y 
-     * @param {number} width 
-     * @param {number} height
-     * @param {number} rotation  
-     */
-    drawFilledRectangle(x, y, width, height, rotation){
+    drawIsoscelesTriangle(x, y, base, height, style, rotation){
+        let p1 = new Point(x - (base / 2), y + (height / 3));
+        let p2 = new Point(x, y - (2 * height / 3));
+        let p3 = new Point(x + (base / 2), y + (height / 3));
+        this.#drawTriangle(x, y, p1, p2, p3, style, rotation);
+    }
+
+    drawRightTriangle(x, y, length, style, rotation){
+        let p1 = new Point(x - (length / 3), y + (length / 3));
+        let p2 = new Point(x - (length / 3), y - (2 * length / 3));
+        let p3 = new Point(x + (2 * length / 3), y + (length / 3));
+        this.#drawTriangle(x, y, p1, p2, p3, style, rotation);
+    }
+
+    drawStar(x, y, length, style = FILLFRAME, rotation, rotateAroundPoint){
         
-        if(rotation){
-            this.canvas.translate(x,y);
-            this.rotateCanvas(rotation);
-            this.canvas.translate(-x,-y);
+        let points = [];
+        for(let i = 0; i < 5; i++){
+            // outer
+            points.push(new Point(x + length*Math.cos((2*Math.PI*i/5)), y + length*Math.sin((2*Math.PI*i/5))));
+
+            // inner
+            points.push(new Point(x + length/3*Math.cos((2*Math.PI*i/5 + 2*Math.PI/10)), y + length/3*Math.sin((2*Math.PI*i/5 + 2*Math.PI/10))));
         }
-        this.canvas.fillRect(x-width/2, y-height/2, width, height);
-        if(rotation) this.resetCanvasRotation();
+        this.drawPolygon(points, style, rotation, rotateAroundPoint);
     }
 
-    /**
-     * Draws a framed rectangle centered at (x,y).
-     * Color is the canvas' strokeStyle.
-     * @param {number} x 
-     * @param {number} y 
-     * @param {number} width 
-     * @param {number} height 
-     */
-    drawFramedRectangle(x, y, width, height, rotation){
-        if(rotation){
-            this.canvas.translate(x,y);
-            this.rotateCanvas(rotation);
-            this.canvas.translate(-x,-y);
-        }
-        this.canvas.strokeRect(x-width/2, y-height/2, width, height);
-        if(rotation) this.resetCanvasRotation();
-    }
-
-    /**
-     * Draws a framed and filled rectangle, centered at (x,y).
-     * Fill color is the canvas' fillStyle.
-     * Frame color is the canvas' strokeStyle.
-     * @param {number} x 
-     * @param {number} y 
-     * @param {number} width 
-     * @param {number} height 
-     */
-    drawFillFramedRectangle(x, y, width, height, rotation){
-        if(rotation){
-            this.canvas.translate(x,y);
-            this.rotateCanvas(rotation);
-            this.canvas.translate(-x,-y);
-        }
-        this.drawFilledRectangle(x, y, width, height);
-        this.drawFramedRectangle(x, y, width, height);
-        if(rotation) this.resetCanvasRotation();
-    }
-
-    drawFilledCircle(x, y, radius){
-        this.canvas.beginPath();
-        this.canvas.arc(x, y, radius, 0, 2*Math.PI);
-        this.canvas.fill();
-    }
-
-    drawFramedCircle(x, y, radius){
-        this.canvas.beginPath();
-        this.canvas.arc(x, y, radius-(this.canvas.lineWidth/2), 0, 2*Math.PI);
-        this.canvas.stroke();
-    }
-
-    drawFillFramedCircle(x, y, radius){
-        this.drawFilledCircle(x, y, radius-(3*this.canvas.lineWidth/4));
-        this.drawFramedCircle(x, y, radius);
-    }
-
-    drawFilledOval(x, y, xRadius, yRadius, rotation){
-        this.canvas.beginPath();
-        this.canvas.ellipse(x, y, xRadius, yRadius, rotation*BearcatGraphics.RADTODEG, 0, 2*Math.PI);
-        this.canvas.fill();
-    }
-
-    drawFramedOval(x, y, xRadius, yRadius, rotation){
-        this.canvas.beginPath();
-        this.canvas.ellipse(x, y, xRadius, yRadius, rotation*BearcatGraphics.RADTODEG, 0, 2*Math.PI);
-        this.canvas.stroke();
-    }
-
-    drawFillFramedOval(x, y, xRadius, yRadius, rotation){
-        this.drawFilledOval(x, y, xRadius, yRadius, rotation);
-        this.drawFramedOval(x, y, xRadius, yRadius, rotation);
-    }
-
-    drawFilledEquilateralTriangle(x, y, length, rotation){
-        let h = Math.sqrt(3*length*length)/2;
-        if(rotation){
-            this.canvas.translate(x, y);
-            this.rotateCanvas(rotation);
-            this.canvas.translate(-x, -y);
+    drawPolygon(points, style, rotation, rotateAroundPoint){
+        if(rotation) {
+            let p = rotateAroundPoint === undefined ? this.findCenter(points) : rotateAroundPoint;
+            this.drawCircle(p.x, p.y, 5)
+            this.#rotateLocal(p.x, p.y, rotation);
         }
         this.canvas.beginPath();
-        this.canvas.moveTo(x-(length/2), y+(h/3));
-        this.canvas.lineTo(x, y-(2*h/3));
-        this.canvas.lineTo(x+(length/2), y+(h/3));
-        this.canvas.lineTo(x-(length/2), y+(h/3));
-        this.canvas.fill();
+        this.canvas.moveTo(points[0].x, points[0].y);
+        for(let i = 1; i < points.length; i++)
+            this.canvas.lineTo(points[i].x, points[i].y);
+        this.canvas.lineTo(points[0].x, points[0].y);
+        // if (style === FRAME || style === FILLFRAME) this.canvas.lineTo(p2.x, p2.y); // prevents jagged p1 corner on stroke
+        if(style === FILL) this.canvas.fill();
+        else if(style === FRAME) this.canvas.stroke();
+        else if(style === FILLFRAME){
+            this.canvas.stroke();
+            this.canvas.fill();
+        }
+        else console.error("Invalid style type: " + style + "; valid style types are FILL, FRAME, and FILLFRAME");
         if(rotation) this.resetCanvasRotation();
     }
 
-    drawFramedEquilateralTriangle(x, y, length, rotation){
-        let h = Math.sqrt(3*length*length)/2;
-        if(rotation){
-            this.canvas.translate(x, y);
-            this.rotateCanvas(rotation);
-            this.canvas.translate(-x, -y);
+    findCenter(points){
+        let xAverage = 0;
+        let yAverage = 0;
+        for(let p of points){
+            xAverage += p.x;
+            yAverage += p.y;
         }
+        return new Point(xAverage/points.length, yAverage/points.length);
+    }
+
+    #drawTriangle(x, y, p1, p2, p3, style, rotation) {
+        if (rotation) this.#rotateLocal(x, y, rotation);
         this.canvas.beginPath();
-        this.canvas.moveTo(x-(length/2), y+(h/3));
-        this.canvas.lineTo(x, y-(2*h/3));
-        this.canvas.lineTo(x+(length/2), y+(h/3));
-        this.canvas.lineTo(x-(length/2), y+(h/3));
-        this.canvas.lineTo(x, y-(2*h/3)); // silly
-        this.canvas.stroke();
-        if(rotation) this.resetCanvasRotation();
-    }
-
-    drawFillFramedEquilateralTriangle(x, y, length, rotation){
-        if(rotation){
-            this.canvas.translate(x, y);
-            this.rotateCanvas(rotation);
-            this.canvas.translate(-x, -y);
+        this.canvas.moveTo(p1.x, p1.y);
+        this.canvas.lineTo(p2.x, p2.y);
+        this.canvas.lineTo(p3.x, p3.y);
+        this.canvas.lineTo(p1.x, p1.y);
+        if (style === FRAME || style === FILLFRAME) this.canvas.lineTo(p2.x, p2.y); // prevents jagged p1 corner on stroke
+        if(style === FILL) this.canvas.fill();
+        else if(style === FRAME) this.canvas.stroke();
+        else if(style === FILLFRAME){
+            this.canvas.stroke();
+            this.canvas.fill();
         }
-        this.drawFilledEquilateralTriangle(x, y, length);
-        this.drawFramedEquilateralTriangle(x, y, length);
-        if(rotation) this.resetCanvasRotation();
+        else console.error("Invalid style type: " + style + "; valid style types are FILL, FRAME, and FILLFRAME");
+        if (rotation) this.resetCanvasRotation();
     }
 
-    drawFilledRightTriangle(x, y, length, rotation){
-        if(rotation){
-            this.canvas.translate(x, y);
-            this.rotateCanvas(rotation);
-            this.canvas.translate(-x, -y);
-        }
-        this.canvas.beginPath();
-        this.canvas.moveTo(x-(length/3), y+(length/3));
-        this.canvas.lineTo(x-(length/3), y-(2*length/3));
-        this.canvas.lineTo(x+(2*length/3), y+(length/3));
-        this.canvas.lineTo(x-(length/3), y+(length/3));
-        this.canvas.fill();
-        if(rotation) this.resetCanvasRotation();
+    #rotateLocal(x, y, rotation) {
+        this.canvas.translate(x, y);
+        this.canvas.rotate(rotation * BearcatGraphics.RADTODEG);
+        this.canvas.translate(-x, -y);
     }
 
-    drawFramedRightTriangle(x, y, length, rotation){
-        if(rotation){
-            this.canvas.translate(x, y);
-            this.rotateCanvas(rotation);
-            this.canvas.translate(-x, -y);
-        }
-        this.canvas.beginPath();
-        this.canvas.moveTo(x-(length/3), y+(length/3));
-        this.canvas.lineTo(x-(length/3), y-(2*length/3));
-        this.canvas.lineTo(x+(2*length/3), y+(length/3));
-        this.canvas.lineTo(x-(length/3), y+(length/3));
-        this.canvas.lineTo(x-(length/3), y-(2*length/3)); // silly
-        this.canvas.stroke();
-        if(rotation) this.resetCanvasRotation();
-    }
+    resetCanvasRotation = () => this.canvas.setTransform(1, 0, 0, 1, 0, 0);
 
-    drawFillFramedRightTriangle(x, y, length, rotation){
-        if(rotation){
-            this.canvas.translate(x, y);
-            this.rotateCanvas(rotation);
-            this.canvas.translate(-x, -y);
-        }
-        this.drawFilledRightTriangle(x, y, length);
-        this.drawFramedRightTriangle(x, y, length);
-        if(rotation) this.resetCanvasRotation();
-    }
+    addEventListener = (type, func) => this.canvasElement.addEventListener(type, func);
 
-    rotateCanvas(degrees){
-        this.canvas.rotate(degrees*BearcatGraphics.RADTODEG);
-    }
+    getMouseX = (e) => e.x - this.canvasElement.getBoundingClientRect().left;
 
-    resetCanvasRotation(){
-        this.canvas.setTransform(1, 0, 0, 1, 0, 0);
-    }
+    getMouseY = (e) => e.y - this.canvasElement.getBoundingClientRect().top;
 
-    addEventListener(type, func, params){
-        this.canvasElement.addEventListener(type, func);
-    }
+    mouseIsInScreen = () => this.mouseX > -1 && this.mouseX <= this.width && this.mouseY > -1 && this.mouseY <= this.height;
 
-    getMouseX(e){
-        return e.x - this.canvasElement.getBoundingClientRect().left;
-    }
-
-    getMouseY(e){
-        return e.y - this.canvasElement.getBoundingClientRect().top;
-    }
-
-    mouseIsInScreen(){
-        return this.mouseX > -1 && this.mouseX <= this.width && this.mouseY > -1 && this.mouseY <= this.height;
-    }
-
-    setUpdateFunction(func){
+    setUpdateFunction(func) {
         this.update = func;
         setInterval(func, 1000 / this.fps);
     }
 
-    getRandomColor(){
+    getRandomColor() {
         let r = Math.floor(Math.random() * 255);
         let g = Math.floor(Math.random() * 255);
         let b = Math.floor(Math.random() * 255);
-        return "#"+r.toString(16)+g.toString(16)+b.toString(16);
+        return "#" + r.toString(16) + g.toString(16) + b.toString(16);
     }
 
-    getRandomInteger(max){
-        return Math.floor(Math.random() * max);
-    }
+    getRandomInteger = (max) => Math.floor(Math.random() * max);
 }
 
 let COLORS = {
@@ -343,10 +260,19 @@ let COLORS = {
     lightpurple: "#E46BFF",
 };
 
-function color(colorString){
-    return COLORS[colorString];
-}
+let FILL = -123456789;
+let FRAME = 123456789;
+let FILLFRAME = 0;
 
-function color(r, g, b){
-    return `#${r.toString(16)}${g.toString(16)}${b.toString(16)}`;
+function color(colorString){return COLORS[colorString];}
+
+function color(r, g, b){`#${r.toString(16)}${g.toString(16)}${b.toString(16)}`;}
+
+class Point{
+    constructor(x, y){
+        this.x = x;
+        this.y = y;
+    }
+
+    distanceTo = (other) => Math.sqrt(((other.y-this.y)*(other.y-this.y))/((other.x-this.x)*(other.x-this.x)));
 }
